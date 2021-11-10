27d078b6301d4eec6c10004da8f51e581a213a55
Masha <you@example.com>
2021-11-10 09:49:49 +0700

    menue added

--- 4.1(((/MainWindow.xaml.cs
+++ 4.1(((/MainWindow.xaml.cs
@@ -24,5 +24,59 @@ namespace _4._1___
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //string a = pervoe.Text;
+            //string b = vtoroe.Text;
+            //int a1 = int.Parse(a);
+            //int b1 = int.Parse(b);
+            //int c = a1 + b1;
+            //int c1 = Convert.ToInt32(c);
+            //vivod.Content=c1;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //string a = pervoe.Text;
+            //string b = vtoroe.Text;
+            //int a1 = int.Parse(a);
+            //int b1 = int.Parse(b);
+            //int c = a1 - b1;
+            //int c1 = Convert.ToInt32(c);
+            //vivod.Content = c1;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            //string a = pervoe.Text;
+            //string b = vtoroe.Text;
+            //int a1 = int.Parse(a);
+            //int b1 = int.Parse(b);
+            //int c = a1 * b1;
+            //int c1 = Convert.ToInt32(c);
+            //vivod.Content = c1;
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            //string a = pervoe.Text;
+            //string b = vtoroe.Text;
+            //double a1 = double.Parse(a);
+            //double b1 = double.Parse(b);
+            //double c = a1/b1;
+            //double c1 = Convert.ToDouble(c);
+            //vivod.Content = c1;
+        }
+
+        private void pervoe_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void vtoroe_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
